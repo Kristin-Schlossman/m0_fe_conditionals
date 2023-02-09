5ee1de78615ed96a24cf821634148f39344c1d98
Kristin Schlossman <Kristin.Schlossman@gmail.com>
2023-02-08 17:11:22 -0800

    update all answer fields and complete challenge.

---
+++ codeChallenges/mild.js
@@ -0,0 +1,10 @@
+var number = 10
+//even numbers are divisble by 2. 
+//even numbers will not have any remainders. 
+//odd numbers will have a reaminder when /2. 
+// % this guy is called modulo operator. 
+
+
+if (number % 2 === 0){
+    console.log("This number is even.");
+} else console.log("This number is odd.")
