dc08e7a21560ab2380e2bf188700df8a352856e7
Kristin Schlossman <Kristin.Schlossman@gmail.com>
2023-02-08 17:14:20 -0800

    complete final review. ready to push.

--- comparisons.js
+++ comparisons.js
@@ -17,30 +17,30 @@ var stringTeachers = "4"
 var numberDogs = 0
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
-console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
+    console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
-
+    console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
 // this should log: false
-
+    console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
-
+    console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
-
+    console.log("is numberStudents greater than or equal to 20?", numberStudents >= 20);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
-
+    console.log("is numberStudents greater than or equal to 21?", numberStudents >= 21);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
-
+    console.log("is numberStudents less than or equal to 20?", numberStudents <= 20);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
-
+    console.log("is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 // #-------------------
 // PART 2: Articulating what you are doing
@@ -53,21 +53,22 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 console.log(4 < 9);
 // YOU DO: Explain.
-
+//this line is comparing intergers. It's asking, "is 4 less than 9?" and will return a T/F value. 
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
-
+//This code is first defining books as a number, and then comparing that value (3) to 4. It will return a value of false, because 4 is not less than 3. 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
-
+//This code set up is defining two variables as numbers and then comparing them. In this case, is 6 larger than 2? Yes, so the answer should be true. 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+// This code is using the number data type, as have the others. This one is asking is the number of attendees exactly equal to the number of meals? In this case, there are more attendees
+// than there are meals. Therefore this should return a value of false, and sounds like an important problem for the caterer. Someone should get on that. 
 
 // #-------------------
 // PART 3: Logical Operators
@@ -78,9 +79,9 @@ var finishedHomework = false;
 
 // EXAMPLE:
 // Determine if the user is hungry and has completed their homework
-console.log(isHungry && finishedHomework);
+    console.log(isHungry && finishedHomework);
 // Determine if the user is hungry or has completed their homework
-console.log(isHungry || finishedHomework);
+    console.log(isHungry || finishedHomework);
 
 var lovesToPlay = true;
 var lovesDogPark = false;
@@ -89,15 +90,23 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+    console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+    console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+    console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
+ if(lovesToPlay && age<2) {
+    console.log("This dog loves to play and is a puppy.");
+ } else if (lovesToPlay && age>2) {
+    console.log("This dog loves to play.")
+ }
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: I struggled with this one a bit, in terms of how to apply what we'd learned about operators and conditionals. 
+// I still feel like my answer is maybe too simple. Should I add an aditional variable that rendors true if the age is less than 2, for instance? 
+// If so, what variable? What would that look like, and how it would it involve puppy? But I decided on the simple, straightforward answer that
+// ticks all the boxes. 
--- selfEval.md
+++ selfEval.md
@@ -1,8 +1,20 @@
 # Self Evaluation
 
 1. How confident do you feel in your understanding and fluency with the comparison operators?
+    I feel confident. They're pretty straightforward. 
 1. How confident do you feel in your understanding and fluency with the logical operators?
+    One of the stumped challenges I tried involved an if statement and ||. I will have to look this over again, and do some more research. Intellectually I understand but translating that into syntax is still difficult for me. Not confident. 
+
 1. How confident do you feel in your understanding and fluency with the if statements?
+    If/else statements take me back to preparing for turing with the logic games, so it feels like stretching a muscle I haven't used in awhile. Though I did learn that it gets easier over time, thinking is this very linear way. The cognition side of things is my biggest hold up right now. I'm slowly reasoning through problems, breaking them down, reading them over and over, sometimes outloud, and it is getting better. 
+
 1. How much time did you spend on this lesson/topic? How did that line up with your time estimate?
+
+    I was feeling very confident with the provided material, but when it came time to do some googling I felt like I was in much deeper water. Goolging however does expose me to the wider langauge of programming, tons of vocab and such that I'm not familiar with yet. I did attempt the harder challenges but couldn't complete, so I have some errors to look into tonight. Overall I feel moderately confident, which is to say at this level, I understand what they're trying to do even when I can't get the syntax right. 
+
 1. What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that?
+
+    I still need to get better about time management. Not only calendar upkeep but also predicting how long assignments will take. I'm finding that I need to look over each assignment to get a feel for the material before commiting to a timeline. That's been a helpful adjustment as I've been able to update my predictions and block out more reasonably as the weeks have progressed. Spoiler: It's taken me about x2 as long as I expected.
+
 1. What questions do you still have? How will you get the answers you need?
+    I have questions about formatting boolean arrays. I will submit a detailed question tonight after some youtube and stack overflow. 
